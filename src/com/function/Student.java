dc71991f6f93f16b194965c3dd465fc496374c5f
yahiya <yahiyakhan85013@gmail.com>
2026-04-13 00:48:39 +0530

    perameter

--- src/com/function/Student.java
+++ src/com/function/Student.java
@@ -6,11 +6,20 @@ public class Student {
     public static void main(String[] args) {
         String Name = StudentName();
         System.out.println("Nmae of student is: "+ Name);
+
+        // perameter
+        String name2 = s2("yahiya");
+        System.out.println("name is: "+name2);
     }
     static String StudentName(){
         Scanner in = new Scanner(System.in);
         System.out.println("Enter name");
-        String name = in.nextLine();
-        return name;
+        return in.nextLine();
     }
+
+    static String s2(String a){
+        return a;
+    }
+
+
 }
