65fe1d5d5448176655554c6e1c4d017675911e2f
yahiya <yahiyakhan85013@gmail.com>
2026-04-16 00:33:06 +0530

    Searching in string

--- src/com/linearSearching/SearchInString.java
+++ src/com/linearSearching/SearchInString.java
@@ -1,4 +1,21 @@
 package com.linearSearching;
 
 public class SearchInString {
+    public static void main(String[] args) {
+        String name= "yahiya";
+        char ch= 'k';
+        boolean ans = search(name,ch);
+        System.out.println(ans);
+    }
+    static boolean search(String name, char ch){
+        if(name.length()==0){
+            return false;
+        }
+        for (int i = 0; i < name.length(); i++) {
+            if (ch==name.charAt(i)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
