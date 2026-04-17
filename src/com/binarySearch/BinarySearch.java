05b75c9defed5f9a5b7b8ce5f1a7d8676290db4f
yahiya <yahiyakhan85013@gmail.com>
2026-04-17 23:52:41 +0530

    binary search

---
+++ src/com/binarySearch/BinarySearch.java
@@ -0,0 +1,26 @@
+package com.binarySearch;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr ={-1,-3,0,1,12,14,15,24,45,67,89,90,100};
+        int target = 24;
+        int ans = binarysearch(arr, target);
+        System.out.println(ans);
+
+    }
+    static int binarysearch(int[] arr, int target){
+        int start =0;
+        int end = arr.length-1;
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            if(target>arr[mid]){
+                start = mid+1;
+            }else if (target<arr[mid]){
+                end =mid -1;
+            }else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
