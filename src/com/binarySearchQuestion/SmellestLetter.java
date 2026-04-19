2fb9ad19a174d59dbf1655416415ba9c7d1d25ae
yahiya <yahiyakhan85013@gmail.com>
2026-04-20 00:36:30 +0530

    smellest letter

---
+++ src/com/binarySearchQuestion/SmellestLetter.java
@@ -0,0 +1,24 @@
+package com.binarySearchQuestion;
+
+public class SmellestLetter {
+    public static void main(String[] args) {
+        char[] letters ={'c','j','f'};
+        char target ='c' ;
+        int ans = nextGreatestLetter(letters, target);
+        System.out.println(ans);
+
+    }
+    static int nextGreatestLetter(char[] letters, char target){
+        int start =0;
+        int end = letters.length-1;
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            if(target<letters[mid]){
+                end = mid-1;
+            }else {
+                start =mid +1;
+            }
+        }
+        return letters[start % letters.length];
+    }
+}
