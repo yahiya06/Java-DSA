388b704d5f5b0cee64f24d28f7e43c02048b208e
yahiya <yahiyakhan85013@gmail.com>
2026-04-22 23:16:59 +0530

    infinite array range find

--- src/com/binarySearchQuestion/InfiniteArray.java
+++ src/com/binarySearchQuestion/InfiniteArray.java
@@ -2,12 +2,16 @@ package com.binarySearchQuestion;
 
 public class InfiniteArray {
     public static void main(String[] args) {
+        int[] arr ={11,2,34,44,56,77,88};
+        int target =56;
+    }
+
+    static int range(int arr[], int target){
+        return 0;
 
     }
 
-    static int binarysearch(int[] arr, int target){
-        int start =0;
-        int end = arr.length-1;
+    static int binarysearch(int[] arr, int target, int start, int end){
         while(start <= end){
             int mid = start + (end-start)/2;
             if(target>arr[mid]){
