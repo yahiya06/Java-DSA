5f5f40e4de352cbdc0238f5132f1084648b89856
yahiya <yahiyakhan85013@gmail.com>
2026-04-24 00:00:34 +0530

    search mountain array

--- src/com/binarySearchQuestion/SearchInMountainArray.java
+++ src/com/binarySearchQuestion/SearchInMountainArray.java
@@ -5,9 +5,29 @@ public class SearchInMountainArray {
 
     }
 
-    static int orderAgnosticBS(int[] arr, int target){
+    static int ans(int[] arr, int target){
+        int peak = peakIndex(arr);
+        int firstry = orderAgnosticBS(arr, target, 0, peak);
+        if(firstry != -1){
+            return firstry;
+        }
+        return orderAgnosticBS(arr, target, peak, arr.length-1);
+    }
+    static int peakIndex(int[] arr){
         int start =0;
         int end = arr.length-1;
+
+        while(start<end){
+            int mid = start + (end-start)/2;
+            if(arr[mid]>arr[mid+1]){
+                end = mid;
+            }else {
+                start= mid +1;
+            }
+        }
+        return start;
+    }
+    static int orderAgnosticBS(int[] arr, int target, int start, int end){
         boolean check = arr[start]<arr[end];// statement check the array is in ascending or descending order
         while(start<=end){
             int mid = start + (end - start)/2;
