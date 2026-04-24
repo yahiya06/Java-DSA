61b85f8595ca33258370444d5cd6058a80197648
yahiya <yahiyakhan85013@gmail.com>
2026-04-25 00:00:17 +0530

    search in matrices

--- src/com/searchInMatrices/RowAndColumnVise.java
+++ src/com/searchInMatrices/RowAndColumnVise.java
@@ -4,4 +4,21 @@ public class RowAndColumnVise {
     public static void main(String[] args) {
 
     }
+
+    static int[] search(int[][] arr, int target){
+        int r = 0;
+        int c = arr.length-1;
+
+        while (r<arr.length && c>=0){
+            if(arr[r][c]==target){
+                return new int[]{arr[r][c]};
+            }
+            if(arr[r][c]<target){
+                r++;
+            }else{
+                c++;
+            }
+        }
+        return new int[]{-1, -1};
+    }
 }
