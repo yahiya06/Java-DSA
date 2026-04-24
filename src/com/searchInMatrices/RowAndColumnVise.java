15eadf876959d53f93ec67d0f63d272cb446cbed
yahiya <yahiyakhan85013@gmail.com>
2026-04-25 00:06:59 +0530

    search in matrices

--- src/com/searchInMatrices/RowAndColumnVise.java
+++ src/com/searchInMatrices/RowAndColumnVise.java
@@ -1,22 +1,30 @@
 package com.searchInMatrices;
 
+import java.util.Arrays;
+
 public class RowAndColumnVise {
     public static void main(String[] args) {
-
+        int [][] arr ={
+                {10,20,30,40},
+                {15,25,35,45},
+                {28,29,37,49},
+                {33,34,38,50}
+        };
+        int target = 37;
+        System.out.println(Arrays.toString(search(arr,target)));
     }
-
     static int[] search(int[][] arr, int target){
         int r = 0;
         int c = arr.length-1;
 
         while (r<arr.length && c>=0){
             if(arr[r][c]==target){
-                return new int[]{arr[r][c]};
+                return new int[]{r,c};
             }
             if(arr[r][c]<target){
                 r++;
             }else{
-                c++;
+                c--;
             }
         }
         return new int[]{-1, -1};
