33c91c32866eb7599dd1f028f0ece5dd4a2d0fca
yahiya <yahiyakhan85013@gmail.com>
2026-04-27 00:53:36 +0530

    bubble sort

--- src/com/sorting/BubbleSort.java
+++ src/com/sorting/BubbleSort.java
@@ -4,19 +4,22 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] nums = {2,3,5,4,1};
+        int[] nums = {1,2,3,4,5};
         bubbleSort(nums);
         System.out.println(Arrays.toString(nums));
     }
     static void bubbleSort(int[] nums){
+        boolean swapped = false;
         for (int i = 0; i < nums.length; i++) {
             for (int j = 1; j < nums.length-i; j++) {
                 if (nums[j]<nums[j-1]){
                     int temp = nums[j];
                     nums[j]= nums[j-1];
                     nums[j-1]=temp;
+                    swapped = true;
                 }
             }
+            if (!swapped) break;
         }
     }
 }
