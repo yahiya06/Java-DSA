e167619a5f2e2d23300261d6b504334ad2a888ea
yahiya <yahiyakhan85013@gmail.com>
2026-04-28 23:08:47 +0530

    insertion sort

--- src/com/sorting/InsertionSort.java
+++ src/com/sorting/InsertionSort.java
@@ -1,7 +1,16 @@
 package com.sorting;
 
+import java.util.Arrays;
+
 public class InsertionSort {
     public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,0,1};
+        insertion(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    private static void insertion(int[] nums) {
 
     }
+
 }
