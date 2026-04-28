3c9f0440d4f3760fd375f05ed971aa4425463f71
yahiya <yahiyakhan85013@gmail.com>
2026-04-28 23:45:10 +0530

    insertion sort

--- src/com/sorting/InsertionSort.java
+++ src/com/sorting/InsertionSort.java
@@ -4,13 +4,21 @@ import java.util.Arrays;
 
 public class InsertionSort {
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4,5,0,1};
+        int[] nums = {5,4,3,2,1,0};
         insertion(nums);
         System.out.println(Arrays.toString(nums));
     }
 
     private static void insertion(int[] nums) {
-
+        for (int i = 0; i <= nums.length-2; i++) {
+            for (int j = i+1; j > 0 ; j--) {
+                if (nums[j]<nums[j-1]){
+                    int temp = nums[j];
+                    nums[j] = nums[j-1];
+                    nums[j-1] = temp;
+                }else break;
+            }
+        }
     }
 
 }
