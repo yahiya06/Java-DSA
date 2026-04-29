e50d840c1329c0c61d0a61f5b5ddcf36e626db5d
yahiya <yahiyakhan85013@gmail.com>
2026-04-29 23:40:00 +0530

    Cycle sort algorithm

--- src/com/sorting/CycleSort.java
+++ src/com/sorting/CycleSort.java
@@ -1,7 +1,28 @@
 package com.sorting;
 
+import java.util.Arrays;
+
 public class CycleSort {
     public static void main(String[] args) {
+        int[] nums = {5,4,3,2,1,0};
+        cycleSort(nums);
+        System.out.println(Arrays.toString(nums));
+    }
 
+    static void cycleSort(int[] nums){
+        int i = 0;
+
+        while (i < nums.length){
+            int correct = nums[i]-1;
+
+            if (nums[i] != correct  ){
+                int temp =nums[i];
+                nums[i] =correct;
+                correct =temp;
+
+            }else{
+               i++;
+            }
+        }
     }
 }
