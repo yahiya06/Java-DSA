198325f47b0f8dd171d5101497907d686a404c50
yahiya <yahiyakhan85013@gmail.com>
2026-04-30 23:51:45 +0530

    leet code 448

--- src/com/SortingQuestion/NumberDisappearedInAnArray.java
+++ src/com/SortingQuestion/NumberDisappearedInAnArray.java
@@ -1,9 +1,18 @@
 package com.SortingQuestion;
 
 import java.util.List;
-
+//leetcode 448
 class NumberDisappearedInAnArray {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        
+        int i =0;
+        while(i<nums.length){
+            int correct = nums[i]-1;
+            if(nums[i] != nums[correct]){
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
+            }else i++;
+        }
+
     }
 }
