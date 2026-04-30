a755e9da1c76bb4bf3e463ca5cd965fa4e4580c3
yahiya <yahiyakhan85013@gmail.com>
2026-04-30 14:46:55 +0530

    contains Duplicate leet code 217

---
+++ src/com/SortingQuestion/Solution.java
@@ -0,0 +1,14 @@
+package com.SortingQuestion;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        for(int i=0;i<=nums.length-1;i++){
+            for(int j=i+1;j<=nums.length-1;j++){
+                if(nums[i]==nums[j]){
+                return true;
+            }
+            }
+        }
+        return false;
+    }
+}
