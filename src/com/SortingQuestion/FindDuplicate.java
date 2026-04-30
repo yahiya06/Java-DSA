fcd32931a69b1ce34f23ae681403918e52a40c14
yahiya <yahiyakhan85013@gmail.com>
2026-05-01 00:41:40 +0530

    leet code 287

--- src/com/SortingQuestion/FindDuplicate.java
+++ src/com/SortingQuestion/FindDuplicate.java
@@ -1,7 +1,12 @@
 package com.SortingQuestion;
 
 class FindDuplicate {
-    public int findDuplicate(int[] nums) {
+    public static void main(String[] args) {
+        int[] nums = {1,3,4,2,2};
+        int ans = findDuplicate(nums);
+        System.out.println(ans);
+    }
+    static public int findDuplicate(int[] nums) {
         int i=0;
         while (i<nums.length){
             if (nums[i] != i+1){
