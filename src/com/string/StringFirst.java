9ee32e1e4d66f6c752ed9fe7be12baa38f2c46ad
yahiya <yahiyakhan85013@gmail.com>
2026-05-02 00:02:34 +0530

    String and stringbuilder

--- src/com/string/StringFirst.java
+++ src/com/string/StringFirst.java
@@ -5,5 +5,13 @@ public class StringFirst {
         String name = "yahiya";
         System.out.println(name);
 
+        //string pool
+        //both pointing to same object in heap string pool
+        String a = "same";
+        String b = "same";
+        System.out.println(a+" "+b);
+        //now a new object is created named not same in heap and a is point to that but b is same
+        a= "not same";
+        System.out.println(a);
     }
 }
