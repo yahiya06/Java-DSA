971bc5126a1f41d47bbaef2aceef9ed742483159
yahiya <yahiyakhan85013@gmail.com>
2026-05-02 00:09:55 +0530

    String comparison

---
+++ src/com/string/Comparison.java
@@ -0,0 +1,12 @@
+package com.string;
+
+import java.util.SplittableRandom;
+
+public class Comparison {
+    public static void main(String[] args) {
+        String a ="yahiya";
+        String b ="yahiya";
+        //give true because pointing to same object
+        System.out.println(a==b);
+    }
+}
