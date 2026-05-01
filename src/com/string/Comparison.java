408b0085b37b558537f08515a9a7806482b66bed
yahiya <yahiyakhan85013@gmail.com>
2026-05-02 00:27:04 +0530

    String Comparison

--- src/com/string/Comparison.java
+++ src/com/string/Comparison.java
@@ -8,5 +8,16 @@ public class Comparison {
         String b ="yahiya";
         //give true because pointing to same object
         System.out.println(a==b);
+
+        //give false because pointing to differ. object
+        String name = new String("name");
+        String name1 =new String("name");
+        System.out.println(name1 == name);
+
+        //give true because value is same
+        String name01 = new String("yahiya");
+        String name02 = new String("yahiya");
+        System.out.println(name01.equals(name02));
+
     }
 }
