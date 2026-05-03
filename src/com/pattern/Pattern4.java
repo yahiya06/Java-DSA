3b4cfe4917e551d08b4197602a65ff1e692246e1
yahiya <yahiyakhan85013@gmail.com>
2026-05-04 00:33:56 +0530

    pattern 4 1 12 123 1234 12345

---
+++ src/com/pattern/Pattern4.java
@@ -0,0 +1,20 @@
+package com.pattern;
+
+//1
+//12
+//123
+//1234
+//12345
+public class Pattern4 {
+    public static void main(String[] args) {
+        pattern4(5);
+    }
+    static void pattern4(int n){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i ; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
