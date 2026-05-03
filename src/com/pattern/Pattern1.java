6f8717f1a5313ebfcebbe76ebc2bbe6b0d1d6618
yahiya <yahiyakhan85013@gmail.com>
2026-05-04 00:08:47 +0530

    pattern * ** *** ****

--- src/com/pattern/Pattern1.java
+++ src/com/pattern/Pattern1.java
@@ -1,12 +1,17 @@
 package com.pattern;
 
+import java.util.Scanner;
+
 //*
 //**
 //***
 //****
 public class Pattern1 {
+    static  Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        for (int i = 1; i <= 4;i++){
+        System.out.println("Enter number ");
+        int num = sc.nextInt();
+        for (int i = 1; i <= num;i++){
             for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
