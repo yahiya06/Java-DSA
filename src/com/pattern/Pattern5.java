8ca50819470de34352d07bbecdd27e7f93c69b75
yahiya <yahiyakhan85013@gmail.com>
2026-05-04 01:04:39 +0530

    pattern 5 * * * * * * * * * * * * * * * * * * * * * * * * *

--- src/com/pattern/Pattern5.java
+++ src/com/pattern/Pattern5.java
@@ -12,11 +12,11 @@ package com.pattern;
 
 public class Pattern5 {
     public static void main(String[] args) {
-        pattern5(5);
+        pattern5(7);
     }
     static void pattern5(int n){
         int row = n;
-        if(row>=n){
+        if(true){
             for (int i = 1; i <=row ; i++) {
                 for (int j = 1; j <=i ; j++) {
                     System.out.print("* ");
@@ -24,7 +24,7 @@ public class Pattern5 {
                 System.out.println();
             }
         }
-        if(row<=n) {
+        if(true) {
             for (int ii = n-1; ii >= 1; ii--) {
                 for (int jj = ii; jj >=1; jj--) {
                     System.out.print("* ");
