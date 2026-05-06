d8f8a907212277304dd246e1270a74f980688197
yahiya <yahiyakhan85013@gmail.com>
2026-05-07 00:35:25 +0530

    FibonacciNumber

--- src/com/basic/FibonacciNumber.java
+++ src/com/basic/FibonacciNumber.java
@@ -2,7 +2,7 @@ package com.basic;
 
 import java.util.Scanner;
 
-public class FabonacciNumber {
+public class FibonacciNumber {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
@@ -19,7 +19,7 @@ public class FabonacciNumber {
 //        System.out.println(b);
 
 //with for loop
-        for(int count=2; count<=n; count++){
+        for(int count = 2; count <= n; count++){
             int temp =b;
             b= a+b;
             a=temp;
--- src/com/recursion/FibonacciNumber.java
+++ src/com/recursion/FibonacciNumber.java
@@ -3,5 +3,14 @@ package com.recursion;
 public class FibonacciNumber {
     public static void main(String[] args) {
 
+        System.out.println(fibo(4));
+
+    }
+    static int fibo(int n){
+        if (n < 2) {
+            return n;
+        }
+
+        return fibo(n-1) + fibo(n-2);
     }
 }
