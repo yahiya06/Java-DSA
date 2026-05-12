467aa59e0eafbbb298db69147af6301fa004dad9
yahiya <yahiyakhan85013@gmail.com>
2026-05-13 00:51:20 +0530

    1 to n

--- src/com/recursion/OneToN.java
+++ src/com/recursion/OneToN.java
@@ -1,15 +1,16 @@
 package com.recursion;
 
-public class Nto1 {
+public class OneToN {
     public static void main(String[] args) {
-        int n = 100;
+        int n =5;
         fun(n);
     }
+
     static void fun(int n){
         if (n == 0){
-            return ;
+            return;
         }
+        fun(n -1);
         System.out.println(n);
-        fun(n-1);
     }
 }
