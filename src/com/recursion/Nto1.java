19cf195580f5e8f25445e49d3d4cabc5d1bf960e
yahiya <yahiyakhan85013@gmail.com>
2026-05-13 00:42:22 +0530

    n to 1

---
+++ src/com/recursion/Nto1.java
@@ -0,0 +1,15 @@
+package com.recursion;
+
+public class Nto1 {
+    public static void main(String[] args) {
+        int n = 5;
+        fun(n);
+    }
+    static void fun(int n){
+        if (n == 0){
+            return ;
+        }
+        System.out.println(n);
+        fun(n-1);
+    }
+}
