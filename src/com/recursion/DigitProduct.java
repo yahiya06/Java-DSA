9f5116c2d76f76a5d73131bee4eae55e45e4803b
yahiya <yahiyakhan85013@gmail.com>
2026-05-14 00:08:43 +0530

    digits Product

---
+++ src/com/recursion/DigitProduct.java
@@ -0,0 +1,20 @@
+package com.recursion;
+
+import java.util.Scanner;
+
+public class DigitProduct {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        int n = sc.nextInt();
+        int ans = product(n);
+        System.out.println(ans);
+    }
+    static int product(int n){
+        if(n==0){
+            return 1;
+        }
+
+        return (n % 10) * product(n/10) ;
+    }
+}
