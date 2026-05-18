65cc19262b730d18725bcd354b74df957ca4812a
yahiya <yahiyakhan85013@gmail.com>
2026-05-18 23:52:29 +0530

    recursion in array linear search

---
+++ src/com/recursionQuestionArray/LinearSearch.java
@@ -0,0 +1,13 @@
+package com.recursionQuestionArray;
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {3,4,6,2,7,34,56,78,23,2};
+        int target = 56;
+        int ans = ls(arr,target);
+        System.out.println(ans);
+    }
+    static int ls(int[] arr, int target){
+        if()
+    }
+}
