95fa85e6b6d3bc745d658b982c9c14cd78be49e8
yahiya <yahiyakhan85013@gmail.com>
2026-05-18 23:44:23 +0530

    recursion in array

--- src/com/recursionQuestionArray/Sorted.java
+++ src/com/recursionQuestionArray/Sorted.java
@@ -2,6 +2,13 @@ package com.recursionQuestionArray;
 
 public class Sorted {
     public static void main(String[] args) {
-
+        int[] arr = {1,2,3,4,5,7,9,12};
+        System.out.println(sort(arr, 0));
+    }
+    static boolean sort(int[] arr, int index){
+        if(index == arr.length-1){
+            return true;
+        }
+        return arr[index] < arr[index+1] && sort(arr,index+1);
     }
 }
