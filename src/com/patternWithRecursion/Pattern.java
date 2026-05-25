24a9bb506836807e02952bdebf792c0832c70ded
yahiya <yahiyakhan85013@gmail.com>
2026-05-25 23:40:35 +0530

    pattern with recursion

--- src/com/patternWithRecursion/Pattern.java
+++ src/com/patternWithRecursion/Pattern.java
@@ -8,6 +8,7 @@ package com.patternWithRecursion;
 public class Pattern {
     public static void main(String[] args) {
         tri(4,0);
+        tri2(4,0);
     }
     static void tri(int row, int col){
         if(row == 0){
@@ -20,6 +21,19 @@ public class Pattern {
         else {
             System.out.println();
             tri(row-1,0);
+        }
+    }
+    static void tri2(int row, int col){
+        if(row == 0){
+            return;
+        }
+        if(col<row){
+            tri2(row,col+1);
+            System.out.print("*");
+        }
+        else {
+            tri2(row-1,0);
+            System.out.println();
         }
     }
 }
