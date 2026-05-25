747c9d2a2ec7089f939961e8450acf3ac6feae83
yahiya <yahiyakhan85013@gmail.com>
2026-05-26 00:19:59 +0530

    selection sort

---
+++ src/com/patternWithRecursion/SelectionSort.java
@@ -0,0 +1,29 @@
+package com.patternWithRecursion;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] nums = {4,3,2,1};
+        selSort(nums,nums.length-1,0,0);
+        System.out.println(Arrays.toString(nums));
+    }
+    static void selSort(int[] nums, int r, int c, int max){
+        if(r ==0){
+            return;
+        }
+
+        if (c<r){
+           if(nums[c] > nums[max]){
+               selSort(nums,r,c+1,c);
+           }else{
+               selSort(nums,r,c+1,max);
+           }
+        }else{
+            int temp = nums[max];
+            nums[r-1] =nums[max];
+            nums[max]=temp;
+            selSort(nums,r-1,0,0);
+        }
+    }
+}
