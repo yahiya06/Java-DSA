ba301649cf59b040035c660053d80034770ac6e6
yahiya <yahiyakhan85013@gmail.com>
2026-06-02 00:22:41 +0530

    quick sort

--- src/com/quickSort/QuickSort.java
+++ src/com/quickSort/QuickSort.java
@@ -4,4 +4,33 @@ public class QuickSort {
     public static void main(String[] args) {
 
     }
+    static void sort(int[] nums, int low, int high){
+        if(low >= high){
+            return;
+        }
+
+        int s = low;
+        int e = high;
+        int mid = s + (e - s)/2;
+        int pivot = nums[mid];
+
+        while (s <= e){
+            while (nums[s] < pivot){
+                s++;
+            }
+            while (nums[e] > pivot ){
+                e--;
+            }
+
+            if(s <= e){
+                int temp = nums[s];
+                nums[s] = nums[e];
+                nums[e] = temp;
+                s++;
+                e--;
+            }
+        }
+        sort(nums, low, e);
+        sort(nums, s, high);
+    }
 }
