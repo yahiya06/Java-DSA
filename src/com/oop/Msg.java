e891b34dbd4d444cdfd9c371ca1e299e72139284
yahiya <yahiyakhan85013@gmail.com>
2026-07-11 00:26:36 +0530

    packages example

---
+++ src/com/oop/Msg.java
@@ -0,0 +1,10 @@
+package com.oop;
+
+import static com.oop.Student.msg;
+
+public class Msg {
+    public static void main(String[] args) {
+        System.out.println("this class");
+        msg();
+    }
+}
--- src/com/oop/Student.java
+++ src/com/oop/Student.java
@@ -12,6 +12,9 @@ public class Student {
         System.out.println(std1.name);
         System.out.println(std1.marks);
     }
+    public static void msg(){
+        System.out.println("hellow from another pkg");
+    }
 }
 
 class StudentInfo{
