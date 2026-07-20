6930a1941a6689c3f0c5b144491617a19b843282
yahiya <yahiyakhan85013@gmail.com>
2026-07-21 01:07:15 +0530

    polymorphisam overloading

---
+++ src/com/oop/polymorphisn/Numbers.java
@@ -0,0 +1,18 @@
+package com.oop.polymorphisn;
+
+public class Numbers {
+    int sum(int a , int b){
+        return a+b;
+    }
+
+    int sum(int a, int b, int c){
+        return a+b+c;
+    }
+
+    public static void main(String[] args) {
+        Numbers obj = new Numbers();
+
+        System.out.println(obj.sum(3,4));
+        System.out.println(obj.sum(4,6,8));
+    }
+}
