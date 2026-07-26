76c6b4c122308115aa539ce1db4911305e399f8f
yahiya <yahiyakhan85013@gmail.com>
2026-07-27 01:15:51 +0530

    ArrayList custom

---
+++ src/com/oop/generics/CustomArrayList.java
@@ -0,0 +1,73 @@
+package com.oop.generics;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class CustomArrayList {
+
+    private int[] data;
+    private static int DEFAULT_SIZE= 10;
+    private int size= 0;
+
+    public CustomArrayList() {
+        this.data = new int[DEFAULT_SIZE];
+    }
+
+    public void add(int num){
+        if (isFull()){
+            resize();
+        }
+        data[size++]=num;
+    }
+
+    private void resize() {
+        int[] temp = new int[DEFAULT_SIZE *2];
+        for (int i = 0; i < data.length; i++) {
+            temp[i]=data[i];
+        }
+        data=temp;
+    }
+
+    private boolean isFull(){
+        return size == data.length;
+    }
+
+    public int remove(){
+        int removed = data[--size];
+        return removed;
+    }
+
+    public int get(int index){
+        return data[index];
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public void set(int index, int value){
+        data[index]= value;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomArrayList{" +
+                "data=" + Arrays.toString(data) +
+                ", size=" + size +
+                '}';
+    }
+
+    public static void main(String[] args) {
+//        ArrayList list = new ArrayList();
+        CustomArrayList list = new CustomArrayList();
+        list.add(23);
+        list.add(24);
+        list.add(25);
+        list.add(25);
+
+        list.remove();
+
+        list.add(90);
+        System.out.println(list);
+    }
+}
