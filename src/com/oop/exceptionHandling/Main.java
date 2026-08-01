295614259185086f0096dfb1dc0c5ac2cad5f69c
yahiya <yahiyakhan85013@gmail.com>
2026-08-02 01:27:04 +0530

    exception handling

---
+++ src/com/oop/exceptionHandling/Main.java
@@ -0,0 +1,14 @@
+package com.oop.exceptionHandling;
+
+public class Main {
+    public static void main(String[] args) {
+        int a= 8;
+        int b= 0;
+        try {
+            int c = a/b;
+            System.out.println(c);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
