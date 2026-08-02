08f034b484717a4a8348b2ff1a274e73ac57e46b
yahiya <yahiyakhan85013@gmail.com>
2026-08-03 00:02:35 +0530

    exception handling

--- src/com/oop/exceptionHandling/Main.java
+++ src/com/oop/exceptionHandling/Main.java
@@ -5,10 +5,17 @@ public class Main {
         int a= 8;
         int b= 0;
         try {
-            int c = a/b;
+//            int c = a/b;
+            int c = devide(3,0);
             System.out.println(c);
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
+    }
+    static int devide(int a, int b){
+        if(b==0){
+            throw new ArithmeticException("do not devide by zero");
+        }
+        return a/b;
     }
 }
