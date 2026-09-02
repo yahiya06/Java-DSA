06704d69e14c5be63659aa767deb80dda32a1e34
yahiya <yahiyakhan85013@gmail.com>
2026-09-03 00:53:34 +0530

    linked list reverse function

--- src/com/linkedList/LL.java
+++ src/com/linkedList/LL.java
@@ -210,4 +210,17 @@ public class LL {
         }
         return false ;
     }
+
+//    public static Node reverseList(Node head) {
+//        Node prev = null;
+//        Node current = head;
+//        Node next;
+//        while(current != null){
+//            next = current.next;
+//            current.next = prev;
+//            prev = current;
+//            current = next;
+//        }
+//        return prev;
+//    }
 }
