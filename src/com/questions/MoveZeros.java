6aa11a8f31c40996970318d778c6cf292ec937b6
yahiya <yahiyakhan85013@gmail.com>
2026-09-05 01:47:54 +0530

    move zeros at end of array

--- src/com/questions/MoveZeros.java
+++ src/com/questions/MoveZeros.java
@@ -1,5 +1,7 @@
 package com.questions;
 
+//https://leetcode.com/problems/move-zeroes/?utm_source=chatgpt.com
+
 public class MoveZeros {
     public static void main(String[] args) {
         int nums[] = {0,1,0,3,12};
