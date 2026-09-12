5c40eafb57452d370ba8c88e21684754f5622999
yahiya <yahiyakhan85013@gmail.com>
2026-09-13 01:42:03 +0530

    custom stack

---
+++ src/com/stackNqueue/CustomStack.java
@@ -0,0 +1,29 @@
+package com.stackNqueue;
+
+public class CustomStack {
+    protected int[] data;
+    private static final int DEFAULT_SIZE=10;
+
+    int ptr =-1;
+
+    public CustomStack(){
+        this(DEFAULT_SIZE);
+    }
+
+    public CustomStack(int size) {
+        this.data = new int[size];
+    }
+
+    public boolean push(int item){
+        if (isFull()){
+            return false;
+        }
+        ptr++;
+        data[ptr]=item;
+        return true;
+    }
+
+    private boolean isFull() {
+        return ptr == data.length-1;
+    }
+}
