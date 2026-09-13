df62598db7eedc6f62de59469cb9157bbfb401e1
yahiya <yahiyakhan85013@gmail.com>
2026-09-14 01:16:59 +0530

    custom stack main

---
+++ src/com/stackNqueue/StackMain.java
@@ -0,0 +1,17 @@
+package com.stackNqueue;
+
+public class StackMain {
+    public static void main(String[] args) throws StackException {
+        CustomStack stack = new CustomStack();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+
+        System.out.println("Removed item is:" + stack.pop());
+        System.out.println("Removed item is:" + stack.pop());
+
+        System.out.println("Peek of stack is:" + stack.peek());
+    }
+}
