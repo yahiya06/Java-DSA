80e43af6d3e398fb1772f75a704960b618e37c8d
yahiya <yahiyakhan85013@gmail.com>
2026-09-15 00:28:45 +0530

    dynamic stack

--- src/com/stackNqueue/StackMain.java
+++ src/com/stackNqueue/StackMain.java
@@ -2,7 +2,8 @@ package com.stackNqueue;
 
 public class StackMain {
     public static void main(String[] args) throws StackException {
-        CustomStack stack = new CustomStack();
+//        CustomStack stack = new CustomStack(3);
+        DynamicStack stack = new DynamicStack(4);
         stack.push(1);
         stack.push(2);
         stack.push(3);
@@ -11,7 +12,9 @@ public class StackMain {
 
         System.out.println("Removed item is:" + stack.pop());
         System.out.println("Removed item is:" + stack.pop());
-
+        System.out.println("Removed item is:" + stack.pop());
+        System.out.println("Removed item is:" + stack.pop());
+        System.out.println("Removed item is:" + stack.pop());
         System.out.println("Peek of stack is:" + stack.peek());
     }
 }
