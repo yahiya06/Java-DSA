2024d20eb1163159ec45d7bc20d50ca2028ad186
yahiya <yahiyakhan85013@gmail.com>
2026-09-20 01:02:28 +0530

    CircularQueue

--- src/com/stackNqueue/QueueMain.java
+++ src/com/stackNqueue/QueueMain.java
@@ -2,22 +2,17 @@ package com.stackNqueue;
 
 public class QueueMain {
     public static void main(String[] args) throws Exception{
-        CustomQueue queue = new CustomQueue();
+//        CustomQueue queue = new CustomQueue();
+        CircularQueue queue = new CircularQueue(5);
         queue.insert(11);
         queue.insert(12);
         queue.insert(13);
         queue.insert(14);
         queue.insert(15);
-        queue.insert(16);
-        queue.insert(17);
-        queue.insert(18);
-        queue.insert(19);
         queue.display();
         System.out.println(queue.remove());
-        System.out.println(queue.remove());
-        System.out.println(queue.remove());
-        System.out.println(queue.remove());
-        System.out.println(queue.remove());
+//        System.out.println(queue.remove());
+//        System.out.println(queue.remove());
         queue.display();
         System.out.println(queue.front());
 
