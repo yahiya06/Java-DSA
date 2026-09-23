01f58c5d91aa39480146f731d7a78424e1571ebf
yahiya <yahiyakhan85013@gmail.com>
2026-09-24 00:42:31 +0530

    min stack

---
+++ src/com/questions/MinStack.java
@@ -0,0 +1,40 @@
+package com.questions;
+
+import java.util.Stack;
+
+public class MinStack {
+
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int value) {
+        stack.push(value);
+        if (minStack.isEmpty()) {
+            minStack.push(value);
+        } else {
+            if (value <= minStack.peek()) {
+                minStack.push(value);
+            } else {
+                minStack.push(minStack.peek());
+            }
+        }
+    }
+
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
