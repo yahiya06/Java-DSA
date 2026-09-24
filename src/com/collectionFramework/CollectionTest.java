46f71ccf95ef5be75c4e5933dd69bd3de3fd9246
yahiya <yahiyakhan85013@gmail.com>
2026-09-25 01:19:02 +0530

    collection framework

---
+++ src/com/collectionFramework/CollectionTest.java
@@ -0,0 +1,15 @@
+package com.collectionFramework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionTest {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(12);
+        list.add(13);
+        list.add(14);
+        System.out.println(list);
+    }
+
+}
