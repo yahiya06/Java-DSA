caa394199a0720af205b1c25689d9f7a25006686
yahiya <yahiyakhan85013@gmail.com>
2026-09-25 00:33:40 +0530

    Array list max element

--- src/com/questions/ArrayListQ.java
+++ src/com/questions/ArrayListQ.java
@@ -15,5 +15,19 @@ public class ArrayListQ {
         list.add(2,25);
         System.out.println(list);
         System.out.println(list.size());
+        System.out.println(large(list));
+    }
+
+    public static int large(ArrayList<Integer> list){
+        int size =list.size();
+        int i= 0;
+        int max = list.get(i);
+        while(i<size){
+            if (list.get(i) > max){
+                max = list.get(i);
+            }
+            i++;
+        }
+        return max;
     }
 }
