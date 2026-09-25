199979344add9e19395b764ffe0cf4695421b95e
yahiya <yahiyakhan85013@gmail.com>
2026-09-26 00:29:51 +0530

    collection framework array list

--- src/com/collectionFramework/CollectionTest.java
+++ src/com/collectionFramework/CollectionTest.java
@@ -10,6 +10,18 @@ public class CollectionTest {
         list.add(13);
         list.add(14);
         System.out.println(list);
+        list.set(1,10);
+        List<Integer> list2 = new ArrayList<>(list);
+        list2.add(123);
+        list2.add(124);
+        System.out.println(list2);
+
+        List<Integer> list3 = new ArrayList<>();
+        list3.addAll(list2);
+        System.out.println(list3);
+
+        System.out.println(list3.indexOf(123));
+
     }
 
 }
